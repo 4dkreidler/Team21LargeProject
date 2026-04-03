a6104366d3fe53db1e0d415ad9d79c8e69efd277
drake k. <160280911+4dkreidler@users.noreply.github.com>
2026-04-03 00:38:25 -0400

    Split code so login and register APIs is in its own file.

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ client.connect();
 app.use(cors());
 app.use(express.json());
 
+//Add Authentication APIs
+var auth = require('./auth.js');
+auth.setApp( app, client );
+
 // ROUTES
 //app.use("/api", authRoutes);
 
@@ -35,59 +39,7 @@ app.use((req, res, next) =>
     next();
 });
 
-app.post('/api/login', async (req, res, next) =>
-{
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
-    var error = '';
-    const { email, password } = req.body;
-    
-    //MongoDB connection
-    const db = client.db('pantry');
-    const results = await
-    db.collection('users').find({email:email,password:password}).toArray();
-    
-    var id = -1;
-    var fn = '';
-    var ln = '';
-    if( results.length > 0 )
-    {
-        id = results[0]._id;
-        fn = results[0].firstName;
-        ln = results[0].lastName;
-    }
 
-    var ret = { id:id, firstName:fn, lastName:ln, error:''};
-    res.status(200).json(ret);
-});
-
-app.post('/api/register', async (req, res, next) =>
-{
-    // incoming: firstName, lastName, email, password
-    // outgoing: id, firstName, lastName, error
-    var error = '';
-    const { firstName, lastName, email, password } = req.body;
-
-    //MongoDB connection
-    const db = client.db('pantry');
-    const existingUser = await db.collection('users').findOne({ email });
-
-    if (existingUser) {
-        return res.status(400).json({ error: "User already exists" });
-    }
-
-    const newUser = {
-        firstName,
-        lastName,
-        email,
-        password
-    };
-
-    const result = await db.collection('users').insertOne(newUser);
-
-    var ret = { id: result.insertedId, firstName: firstName, lastName: lastName, error: '' };
-    res.status(200).json(ret);
-});
 
 //Start server at Port 5000
 app.listen(5000, ()=> {
