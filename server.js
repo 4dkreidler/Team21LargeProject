fc7111eca829198a2b22cbee4fcfeca17b9d0537
SlyeJeremiah <155405463+SlyeJeremiah@users.noreply.github.com>
2026-04-03 00:24:55 -0400

    Added tested and working register api

--- server.js
+++ server.js
@@ -61,6 +61,34 @@ app.post('/api/login', async (req, res, next) =>
     res.status(200).json(ret);
 });
 
+app.post('/api/register', async (req, res, next) =>
+{
+    // incoming: firstName, lastName, email, password
+    // outgoing: id, firstName, lastName, error
+    var error = '';
+    const { firstName, lastName, email, password } = req.body;
+
+    //MongoDB connection
+    const db = client.db('pantry');
+    const existingUser = await db.collection('users').findOne({ email });
+
+    if (existingUser) {
+        return res.status(400).json({ error: "User already exists" });
+    }
+
+    const newUser = {
+        firstName,
+        lastName,
+        email,
+        password
+    };
+
+    const result = await db.collection('users').insertOne(newUser);
+
+    var ret = { id: result.insertedId, firstName: firstName, lastName: lastName, error: '' };
+    res.status(200).json(ret);
+});
+
 //Start server at Port 5000
 app.listen(5000, ()=> {
     console.log(`App is listenig to port : 5000`);
