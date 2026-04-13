29d64bb6f9cfed37dd23bad5403963505bd5db3e
Simon <simon.starhive@proton.me>
2026-04-13 01:18:59 -0400

    imports added

--- Frontend/src/pages/PasswordChange.tsx
+++ Frontend/src/pages/PasswordChange.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import { Card } from "../components/Card";
 import { Layout } from "../components/Layout";
+import { buildPath } from '../utils/Path'; 
 
 const PasswordChange: React.FC = () => {  
     return (
