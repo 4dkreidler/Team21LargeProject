f19fa289cd29a6b857e3e2e657a2a71f0c40dbb9
Simon <simon.starhive@proton.me>
2026-04-16 11:30:13 -0400

    layout fixedscreens fixed

--- app/lib/widgets/layout.dart
+++ app/lib/widgets/layout.dart
@@ -8,13 +8,20 @@ class Layout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
+      backgroundColor: Color(0xFFF9FAFB),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: CustomNavbar(),
+      ),
       body: Center(
+        child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 1200),
         child: Padding(
-          padding: EdgeInsets.all(16),
-          child: child,
+          padding: EdgeInsets.all(24),
+          child: child, // page content
         ),
       ),
-    );
+    ),
+  );
   }
 }
