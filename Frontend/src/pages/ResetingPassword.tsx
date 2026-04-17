e559f994681670758b53377b806719abd75ddc10
Collesha Mighty <c.mighty21@gmail.com>
2026-04-16 20:49:42 -0400

    Add password validation regex to Signup and ResetingPassword components

--- Frontend/src/pages/ResetingPassword.tsx
+++ Frontend/src/pages/ResetingPassword.tsx
@@ -18,9 +18,13 @@ const ResetingPassword: React.FC = () => {
 
         setError("");
 
-        
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
         if (newPassword !== confirmPassword) {
             setError("Passwords do not match.");
+            return;
+        }
+        if(!passwordRegex.test(newPassword)) {
+            setError("Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.");
             return;
         }
 
